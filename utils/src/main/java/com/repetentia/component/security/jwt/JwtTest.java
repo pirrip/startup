7cc3dbf876d6e6ff2edb758ae43ee6320645edc8
pirrip <pirrip@naver.com>
2021-03-29 09:42:29 +0900

    aa

--- utils/src/main/java/com/repetentia/component/security/jwt/JwtTest.java
+++ utils/src/main/java/com/repetentia/component/security/jwt/JwtTest.java
@@ -4,6 +4,13 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 public class JwtTest {
     public static void main(String[] args) {
-        Jwt.withTokenValue(null);
+        String token = "{\"user\":\"pirrip\"}";
+        Jwt.Builder builder = Jwt.withTokenValue(token);
+        builder.header("set", "as");
+        builder.claim("id", "myryne");
+        Jwt jwt = builder.build();
+
+        System.out.println(jwt.toString());
+
     }
 }
